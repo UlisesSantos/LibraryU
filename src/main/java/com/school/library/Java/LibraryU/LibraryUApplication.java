a2d19165b8c90bcb9cb2d500f58d9fdc516aae82
Ulises <ulises.mor.santos@gmail.com>
2025-04-18 14:47:53 -0700

    first commit

---
+++ src/main/java/com/school/library/Java/LibraryU/LibraryUApplication.java
@@ -0,0 +1,13 @@
+package com.school.library.Java.LibraryU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryUApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/school/library/Java/LibraryU/LibraryUApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.library.Java.LibraryU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
